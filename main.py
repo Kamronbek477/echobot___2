214a4e7146635b87609d836e59fcf306ba10bbb5
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2023-05-04 14:14:00 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+def start(update: Update, context: CallbackContext):
+    first_name = update.message.from_user.first_name
+    chat_id = update.message.chat.id
+    bot = context.bot
+    bot.send_message(chat_id,text=f"Assalomu aleyko'm, 👋{first_name}")
+
+def msg(update: Update, context: CallbackContext):
+    chat_id = update.message.chat.id
+    text = update.message.text
+    bot = context.bot
+    bot.send_message(chat_id,text)
